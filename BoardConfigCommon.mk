68db43b8232e92d12711f4cb2add137fb421adda
FirePower36v <75525177+FirePower36v@users.noreply.github.com>
2026-02-19 12:46:12 +0100

    Update BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -74,6 +74,7 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/configs/config.fs
 # Display
 SOONG_CONFIG_qtidisplay_target_needs_raw10_buffer_fix := true
 TARGET_USES_ION := true
+SOONG_CONFIG_qtidisplay_gralloc4 := true
 
 # GPS
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
